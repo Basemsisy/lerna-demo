1d10b82b7038445fe89294bac52dbd66bf4b4e85
Basem Elsisy <basemsisy@gmail.com>
2024-01-25 17:15:58 +0300

    Initial commit

---
+++ packages/button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ButtonProps {
+  children: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+export default Button;
